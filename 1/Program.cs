7a9eff48e87443a8f444b57ccdc57ec08a0b7450
DianaKoshlakova <di.koshlakova.nkv@yandex.ru>
2023-03-16 22:21:03 +0300

    commit

---
+++ 1/Program.cs
@@ -0,0 +1,28 @@
+﻿//Задайте значение N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от N до 1. 
+//Выполнить с помощью рекурсии.
+
+//N = 5 -> "5, 4, 3, 2, 1"
+//N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+using System;
+using static System.Console;
+
+Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+int count = 1;
+
+AllNaturals(number, count);
+
+void AllNaturals(int n, int count)
+{
+    if (count > n)
+    {
+        return;
+    }
+    else
+    {
+        AllNaturals(n, count + 1);
+        Console.Write(count + " ");
+    }
+}
